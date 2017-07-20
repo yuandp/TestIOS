a7242f38b9f21418a9f9f71f9aff5ac50b140c31
george <george@ablesms.com>
2017-07-20 14:26:06 +0800

    111

--- ABLETesGit/ViewController.m
+++ ABLETesGit/ViewController.m
@@ -18,10 +18,14 @@
     [super viewDidLoad];
     
     NSLog(@"===33333");
+    [self stre];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-
+-(void)stre
+{
+    
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
